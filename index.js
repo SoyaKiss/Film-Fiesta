85a57dc054e9c0894c92a7c9fdaee71863e46007
Orsolya <orsolya.kiss.design@gmail.com>
2024-06-17 11:29:07 -0400

    updated signup endpoint

--- index.js
+++ index.js
@@ -7,12 +7,6 @@ let app = express();
 let Movies = Models.Movie;
 let Users = Models.User;
 
-// mongoose
-//   .connect("mongodb://localhost:27017/[film-fiestaDB]", {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//   })
-
 mongoose
   .connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
@@ -131,59 +125,44 @@ app.put(
   }
 );
 
-// POST - Add a movie to the user's favorites
-// app.post(
-//   "/Users/:Username/favorites/:movieTitle",
-//   passport.authenticate("jwt", { session: false }),
-//   async (req, res) => {
-//     try {
-//       const { Username, movieTitle } = req.params;
-
-//       const movie = await Movies.findOne({ Title: movieTitle });
-
-//       if (!movie) {
-//         return res.status(400).send({ message: "No such movie" });
-//       }
-
-//       await Users.findOneAndUpdate(
-//         { Username },
-//         { $addToSet: { favoriteMovies: movie._id } },
-//         { new: true }
-//       )
-//         .then((updatedUser) => {
-//           res.json(updatedUser);
-//         })
-//         .catch((err) => {
-//           console.error(err);
-//           res.status(500).send("Error: " + err);
-//         });
-//     } catch (error) {
-//       console.error(error);
-//       res.status(500).send("Internal Server Error");
-//     }
-//   }
-// );
-
+// POST endpoint to create a user:
 app.post(
-  "/Users/:Username/favorites/:movieId",
-  passport.authenticate("jwt", { session: false }),
+  "/users",
+  [
+    check("Username", "Username is required").isLength({ min: 5 }),
+    check(
+      "Username",
+      "Username contains non-alphanumeric characters - not allowed."
+    ).isAlphanumeric(),
+    check("Password", "Password is required").not().isEmpty(),
+    check("Email", "Email does not appear to be valid").isEmail(),
+  ],
   async (req, res) => {
-    const { Username, movieId } = req.params;
-    const movie = await Movies.findById(movieId);
-    if (!movie) {
-      return res.status(400).send("Movie not found");
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.array() });
     }
-    await Users.findOneAndUpdate(
-      { Username },
-      { $addToSet: { favoriteMovies: movie._id } },
-      { new: true }
-    )
-      .populate("favoriteMovies")
-      .then((user) => res.json(user))
-      .catch((err) => {
-        console.error(err);
-        res.status(500).send("Error updating favorites: " + err);
+
+    const hashedPassword = Users.hashPassword(req.body.Password);
+    try {
+      const user = await Users.findOne({ Username: req.body.Username });
+      if (user) {
+        return res
+          .status(400)
+          .send({ message: req.body.Username + " already exists" });
+      }
+      const newUser = await Users.create({
+        Username: req.body.Username,
+        Password: hashedPassword,
+        Email: req.body.Email,
+        FullName: req.body.FullName,
+        Birthday: req.body.Birthday,
       });
+      res.status(201).json(newUser);
+    } catch (error) {
+      console.error(error);
+      res.status(500).send("Error: " + error);
+    }
   }
 );
 
