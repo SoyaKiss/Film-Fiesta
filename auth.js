9d249d9f31f9eaa84b716817d3332ab8031813a6
SoyaKiss <157721201+SoyaKiss@users.noreply.github.com>
2024-07-06 15:15:43 -0400

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,33 +1,60 @@
-let jwtSecret = "your_jwt_secret";
+// let jwtSecret = "your_jwt_secret";
 
-let jwt = require("jsonwebtoken"),
-  passport = require("passport");
+// let jwt = require("jsonwebtoken"),
+//   passport = require("passport");
 
-require("./passport");
+// require("./passport");
 
-let generateJWTToken = (user) => {
-  return jwt.sign(user, jwtSecret, {
-    subject: user.Username,
-    expiresIn: "7d",
-    algorithm: "HS256",
-  });
+// let generateJWTToken = (user) => {
+//   return jwt.sign(user, jwtSecret, {
+//     subject: user.Username,
+//     expiresIn: "7d",
+//     algorithm: "HS256",
+//   });
+// };
+// module.exports = (router) => {
+//   router.post("/login", (req, res) => {
+//     passport.authenticate("local", { session: false }, (error, user, info) => {
+//       if (error || !user) {
+//         return res.status(400).json({
+//           message: "Something is not right",
+//           user: user,
+//         });
+//       }
+//       req.login(user, { session: false }, (error) => {
+//         if (error) {
+//           res.send(error);
+//         }
+//         let token = generateJWTToken(user.toJSON());
+//         return res.json({ user, token });
+//       });
+//     })(req, res);
+//   });
+// };
+
+const jwt = require('jsonwebtoken');
+const passport = require('passport');
+const { Strategy: JwtStrategy, ExtractJwt } = require('passport-jwt');
+const Models = require('./models.js');
+const Users = Models.User;
+
+const secretKey = 'your_secret_key';
+
+const jwtOptions = {
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+  secretOrKey: secretKey,
 };
-module.exports = (router) => {
-  router.post("/login", (req, res) => {
-    passport.authenticate("local", { session: false }, (error, user, info) => {
-      if (error || !user) {
-        return res.status(400).json({
-          message: "Something is not right",
-          user: user,
-        });
-      }
-      req.login(user, { session: false }, (error) => {
-        if (error) {
-          res.send(error);
-        }
-        let token = generateJWTToken(user.toJSON());
-        return res.json({ user, token });
-      });
-    })(req, res);
-  });
+
+passport.use(
+  new JwtStrategy(jwtOptions, (jwt_payload, done) => {
+    Users.findById(jwt_payload.id, (err, user) => {
+      if (err) return done(err, false);
+      if (user) return done(null, user);
+      return done(null, false);
+    });
+  })
+);
+
+module.exports = (app) => {
+  app.use(passport.initialize());
 };
